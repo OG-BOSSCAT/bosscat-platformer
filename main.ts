8dc6c62fe22d9a3f205a2a61c3df841ecf437467
BOSSCAT <88299467+OG-BOSSCAT@users.noreply.github.com>
2021-12-09 19:04:40 -0800

    Add BOSSCAT animations

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.over(false, effects.splatter)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Dog`, function (sprite, location) {
+    Dog = sprites.create(assets.tile`Dog`, SpriteKind.Enemy)
     info.changeLifeBy(-1)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -10,9 +16,84 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Food`, function (sprite, location) {
     game.over(true, effects.hearts)
 })
+let Dog: Sprite = null
 let BOSSCAT: Sprite = null
 scene.setBackgroundColor(9)
 BOSSCAT = sprites.create(assets.image`BOSSCAT`, SpriteKind.Player)
+animation.runImageAnimation(
+BOSSCAT,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . f . . f . . . . 
+    . . . . . . . . f f f f . . . . 
+    f f . . . . . . f 5 f 5 . . . . 
+    f . . . . . . . f f f f . . . . 
+    f f . . . . . . f f f . . . . . 
+    . f f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . f . . . . . . . . . f . . . . 
+    f . . . . . . . . . . . f . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . f . . f . . . . 
+    . . . . . . . . f f f f . . . . 
+    f f . . . . . . f 5 f 5 . . . . 
+    f . . . . . . . f f f f . . . . 
+    f f . . . . . . f f f . . . . . 
+    . f f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . f . f . . . . f . . f . . . . 
+    . . . . f . . f . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . f . . f . . . . 
+    . . . . . . . . f f f f . . . . 
+    f f . . . . . . f 5 f 5 . . . . 
+    f . . . . . . . f f f f . . . . 
+    f f . . . . . . f f f . . . . . 
+    . f f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . f . f . . . . f . . f . . . . 
+    f . . . f . . f . . . . f . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . f . . f . . . . 
+    . . . . . . . . f f f f . . . . 
+    f f f . . . . . f 5 f 5 . . . . 
+    f . f f . . . . f f f f . . . . 
+    f . f f f f f f f f f . . . . . 
+    f . f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . . f . . . . f . . . . . . . 
+    . . . . f . . f . . . . . . . . 
+    `],
+500,
+true
+)
 BOSSCAT.ay = 200
 controller.moveSprite(BOSSCAT, 100, 100)
 info.setLife(9)
